234b5f29cc4bb755b085416ae74f974a241ef1bb
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-01-23 21:51:49 +0100

    add missing enable extension

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -e
 python setup.py build
 pip install -e .
 jupyter nbextension install --py --symlink --sys-prefix lineup_widget
+jupyter nbextension enable lineup_widget --py --sys-prefix
 
 jupyter notebook
