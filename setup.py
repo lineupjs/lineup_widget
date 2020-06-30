2770120ed2fbad7102a2226b1f6ec8da7a480aa2
Samuel Gratzl <sam@sgratzl.com>
2020-06-30 13:25:45 +0200

    release v4.0.0

--- setup.py
+++ setup.py
@@ -186,12 +186,11 @@ setup_args = {
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: MIT License',
     'Topic :: Multimedia :: Graphics',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
 }
 
