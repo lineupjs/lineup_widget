8cd60dddf45259eb392a610d99ed8bd69766e92d
Samuel Gratzl <sam@sgratzl.com>
2022-08-09 21:07:51 -0400

    fix: build

--- src/plugin.ts
+++ src/plugin.ts
@@ -7,7 +7,7 @@ import { Widget } from '@phosphor/widgets';
 
 import { IJupyterWidgetRegistry } from '@jupyter-widgets/base';
 
-import * as widgetExports from './widget';
+import { LineUpModel, LineUpView, TaggleModel, TaggleView } from './widget';
 
 import { MODULE_NAME, MODULE_VERSION } from './version';
 
@@ -37,6 +37,11 @@ function activateWidgetExtension(
   registry.registerWidget({
     name: MODULE_NAME,
     version: MODULE_VERSION,
-    exports: widgetExports,
+    exports: {
+      LineUpModel,
+      LineUpView,
+      TaggleModel,
+      TaggleView,
+    },
   });
 }
